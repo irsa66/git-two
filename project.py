b25c4332af031fa23aac5c06ae0493f39ebaba58
irsa66 <irsa66@users.noreply.github.com>
2020-08-07 15:53:03 +0500

    conducted initial analysis

---
+++ project.py
@@ -0,0 +1,1 @@
+import pandas as pd import numpy as npimport matplotlib.pyplot as pltdf = pd.read_csv('CBI_clean.csv')df.head()pd.get_option("display.max_columns")df.shapetop_5 = df.head()
